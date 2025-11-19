c2a2c4d831c50a4337b8d5c62d2fa345d929ca37
cloudsgoblack <ellinooratammi@gmail.com>
2025-11-19 14:01:13 +0200

    Muokattu vielä vite.configia

--- vko4/vite.config.ts
+++ vko4/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pilvipalvelut/vko4/dist/',
+  base: '/pilvipalvelut/vko4/',
 })
