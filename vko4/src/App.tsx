85d8b91c3125d41e8495641c6fbb19ade3e9a6fc
Elli <ellinooratammi@gmail.com>
2025-11-19 23:22:04 +0200

    En saanut paluulinkkiä toimimaan, luovutin

--- vko4/src/App.tsx
+++ vko4/src/App.tsx
@@ -5,9 +5,6 @@ function App() {
   return (
     <div className="App">
       <h1>Tehtävä, viikko 4</h1>
-      <p>
-        <a href="https://cloudsgoblack.github.io/pilvipalvelut/"> ← Palaa edelliselle sivulle</a>
-      </p>
       <LoginForm />
     </div>
   )
