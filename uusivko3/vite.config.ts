3ce3df52e58c331e1d9bbef39fd6cd28b545a965
Elli <ellinooratammi@gmail.com>
2025-11-25 23:30:05 +0200

    Delete uusivko3 directory

--- uusivko3/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/pilvipalvelut/uusivko3/',
-  plugins: [react()],
-})
