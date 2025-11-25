7890105f5f00874d56f83ae426e74919106f0b7c
cloudsgoblack <ellinooratammi@gmail.com>
2025-11-25 23:07:33 +0200

    Korjattu base

--- uusivko3/vite.config.ts
+++ uusivko3/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pilvipalvelut/vko3/',
+  base: '/pilvipalvelut/uusivko3/',
   plugins: [react()],
 })
