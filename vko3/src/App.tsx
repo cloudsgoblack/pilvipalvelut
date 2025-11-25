233932babf5895b926ceb8753f706254f3c74ff2
cloudsgoblack <ellinooratammi@gmail.com>
2025-11-25 23:56:59 +0200

    Luotu uusi vko3-projekti ja buildattu

--- vko3/src/App.tsx
+++ vko3/src/App.tsx
@@ -1,65 +1,34 @@
 import { useState, useEffect, useCallback } from 'react'
-import type React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-// Asetetaan cookie
-const setCookie = (name: string, value: string, days: number) => {
-  const expires = new Date(Date.now() + days * 864e5).toUTCString()
-  document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires}; path=/`
-}
-
-// Luetaan cookie
-const getCookie = (name: string): number => {
-  const value = `; ${document.cookie}`
-  const parts = value.split(`; ${name}=`)
-
-  if (parts.length === 2) {
-    const num = parseInt(
-      decodeURIComponent(parts.pop()?.split(';').shift() || ''),
-      10
-    )
-    return isNaN(num) ? 0 : num
-  }
-  return 0
-}
-
 function App() {
-  const [count, setCount] = useState<number>(getCookie('Count') || 0)
+  const [count, setCount] = useState(0)
 
-  const handleClick = useCallback<React.MouseEventHandler<HTMLButtonElement>>(
-    (event) => {
-      console.log('Toiminto(X, Y): ' + event.pageX + ' ' + event.pageY + ')')
-      setCount((prev) => prev + 1)
-    },
-    [] // setCookiea ei tarvita riippuvuudeksi
-  )
+  const handleClick = useCallback<React.MouseEventHandler<HTMLButtonElement>>((event) => {
+    console.log('Toiminto(X, Y): (' + event.pageX + ', ' + event.pageY + ')');
+    setCount((count) => count + 1);
+  }, [setCount]);
 
   useEffect(() => {
-    setCookie('Count', String(count), 7)
-  }, [count])
-
-  useEffect(() => {
-    console.log('Viesti efektifunktiosta')
-  }, [])
+    console.log('Viesti efkektifunktiosta.')
+  });
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank" rel="noopener">
+        <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-
-        <a href="https://react.dev" target="_blank" rel="noopener">
+        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-
-      <h1>Otsikko</h1>
+      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={handleClick}>
-          Laskurin arvo {count}
+        <button onClick={(handleClick)}>
+          count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
