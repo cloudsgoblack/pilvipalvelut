263c1dfadce3ccd29a0c2777d78daf3f5f4db962
cloudsgoblack <ellinooratammi@gmail.com>
2025-12-04 18:50:39 +0200

    Uudelleen tehty taaas koska lakkasi toimimasta

--- vko5/src/App.tsx
+++ vko5/src/App.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useState } from 'react'
 import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import type React from 'react';
 import './App.css'
 
@@ -35,7 +36,7 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
-          <img src="vite.svg" className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
