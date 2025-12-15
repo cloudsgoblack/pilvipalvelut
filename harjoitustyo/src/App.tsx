c51242d816f031c0f9e3a337fd9a62a17e1c9806
cloudsgoblack <ellinooratammi@gmail.com>
2025-12-16 01:35:21 +0200

    Lisätty polku BrowserRouterille

--- harjoitustyo/src/App.tsx
+++ harjoitustyo/src/App.tsx
@@ -9,7 +9,7 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/pilvipalvelut/harjoitustyo">
       <div className="App">
         <Header />
         <Routes>
