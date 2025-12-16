57f55df580aa191bafd7eb99eb661a837b5317e4
cloudsgoblack <ellinooratammi@gmail.com>
2025-12-16 02:22:43 +0200

    Buildasin eri kansioon heh

--- harjoitustyo/vite.config.ts
+++ harjoitustyo/vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: "/pilvipalvelut/harjoitustyo/",
+  build: {
+    outDir: "../docs/harjoitustyo",
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
       "/api": {
